e22e9a568d62a39eab770324682b6a004414d6c1
JoshuaxLasley <jlasley@ftstudent.org>
2025-02-13 12:59:07 -0500

    Cide for player bullet but should work for the enemy

--- scripts/Enemies_Scripts/enemy_attack_for_experiment.gd
+++ scripts/Enemies_Scripts/enemy_attack_for_experiment.gd
@@ -11,7 +11,15 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 # detecting what our bullet collide with
-func _on_area_2d_body_entered(body):
-	if body.name != player:
+#func _on_area_2d_body_entered(body):
+	#if body.name != player:
 		# changing our bullet image into exposion
-		
+		#tecture = load("image")
+		# stoping the process function so that, it doesn't move.
+		#set_process(false)
+		# reducing the health of the body ( player )
+		#body.take_damage()
+		# waiting for 0.2 seconds
+		#await get_tree().create_timer(0.2).timeout
+		# deleting bullet
+		#queue_free()
