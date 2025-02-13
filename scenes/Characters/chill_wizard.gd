93cb9e6d6fa31f2e519df488282d15845449e118
solomonSpence07 <sspence2@ftstudent.org>
2025-02-13 12:59:22 -0500

    still fixing it

--- scenes/Characters/chill_wizard.gd
+++ scenes/Characters/chill_wizard.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
- 
+
 @export var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("left","right","up","down")
-	velocity = input_direction * speed 
+	var input_direction = Input.get_vector("p1_left", "p1_right", "p1_up", "p1_down")
+	velocity = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
