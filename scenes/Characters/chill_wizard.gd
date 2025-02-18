0e91f66a8b71086e041db5aaaf57edc391f31c86
solomonSpence07 <sspence2@ftstudent.org>
2025-02-18 13:14:49 -0500

    only left and right movement

--- scenes/Characters/chill_wizard.gd
+++ scenes/Characters/chill_wizard.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 @export var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("p1_left", "p1_right", "p1_up", "p1_down")
-	velocity = input_direction * speed
+	var input_direction = Input.get_action_strength("p1_right") - Input.get_action_strength("p1_left") #left and Right movement
+	velocity = Vector2(input_direction * speed, 0)  # no vert movement
 
 func _physics_process(delta):
 	get_input()
