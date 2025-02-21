e5f1709eaa7e4e4408c7f02cdbcf046b4b00c55b
graceson <gwheatley@ftstudent.org>
2025-02-21 10:40:22 -0500

    added protection for overclicking, possibly fixed a visual bug on lower-end systems

--- scripts/ui_scripts/main_menu.gd
+++ scripts/ui_scripts/main_menu.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var click_effect: AudioStreamPlayer = $AudioStreamPlayer
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
+var is_transitioning = false
+
 
 func _ready():
 	animation.play("fade_out")
@@ -12,12 +14,15 @@ func _on_start_button_pressed() -> void:
 
 
 func _on_setttings_button_pressed() -> void:
-	click_effect.play()
-	print("settings button pressed")
-	animation.play("fade_in")
-	await $AudioStreamPlayer.finished
-	animation.play("RESET")
-	get_tree().change_scene_to_file("res://scenes/ui/menus/settings_menu.tscn")
+	if is_transitioning == true:
+		return
+	else:
+		is_transitioning = true
+		click_effect.play()
+		print("settings button pressed")
+		animation.play("fade_in")
+		await $AudioStreamPlayer.finished
+		get_tree().change_scene_to_file("res://scenes/ui/menus/settings_menu.tscn")
 
 func _on_exit_button_pressed() -> void:
 	click_effect.play()
--- scripts/ui_scripts/settings_menu.gd
+++ scripts/ui_scripts/settings_menu.gd
@@ -4,15 +4,20 @@ extends Control
 @onready var click_effect: AudioStreamPlayer = $AudioStreamPlayer
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
+var is_transitioning
+
 func _ready():
 	animation.play("fade_out")
 	await $AnimationPlayer.animation_finished
-	animation.play("RESET")
 
 
 
 func _on_back_button_pressed() -> void:
-	click_effect.play()
-	animation.play("fade_in")
-	await $AudioStreamPlayer.finished
-	get_tree().change_scene_to_file("res://scenes/ui/menus/main_menu.tscn")
+	if is_transitioning == true:
+		return
+	else:
+		is_transitioning = true
+		click_effect.play()
+		animation.play("fade_in")
+		await $AudioStreamPlayer.finished
+		get_tree().change_scene_to_file("res://scenes/ui/menus/main_menu.tscn")
