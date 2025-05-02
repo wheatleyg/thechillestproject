0becb02ab557c878882de9ca8c2c17d59a05accc
JoshuaxLasley <jlasley@ftstudent.org>
2025-05-02 12:22:07 -0400

    stuff

--- player_attack_2.gd
+++ player_attack_2.gd
@@ -1,7 +1,9 @@
 extends Player_attack
 
+var speed = 2000
+
 func _ready() -> void:
-	velocity.y = -SPEED
+	velocity.y = -speed
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
