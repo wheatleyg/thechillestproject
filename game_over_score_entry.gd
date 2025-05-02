ad638c46e111a667e3d3104e0e982b94535f4bd3
graceson <gwheatley@ftstudent.org>
2025-05-02 15:27:33 -0400

    i made it work

--- game_over_score_entry.gd
+++ game_over_score_entry.gd
@@ -4,17 +4,17 @@ extends Control
 @onready var spot_1: Label = %spot1
 @onready var spot_2: Label = $Panel/GridContainer/spot2
 @onready var spot_3: Label = $"Panel/GridContainer/spot 3"
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
 var max_letters = len(alphabet)
 var current_letter = 0
 var curr_spot = 0
 var flash = false
-var curr_name = ['_', '_', '_']
+var curr_name = []
 
-var updated = false
 
 
 var curr_name_index =  [0, 0, 0]
@@ -27,97 +27,94 @@ var opaque = Color8(255,255,255,255)
 var half_opaque = Color8(255,255,255,127)
 
 
+const nono_words = ['ass', 'fuc', 'fuk', 'fuq', 'fux', 'fck', 'coc', 'cok', 'coq', 'kox', 'koc', 'kok', 'koq', 'cac', 'cak', 'caq', 'kac', 'kak', 'kaq', 'dic', 'dik', 'diq', 'dix', 'dck', 'pns', 'psy', 'fag', 'fgt', 'ngr', 'nig', 'cnt', 'knt', 'sht', 'dsh', 'twt', 'bch', 'cum', 'clt', 'kum', 'klt', 'suc', 'suk', 'suq', 'sck', 'lic', 'lik', 'liq', 'lck', 'jiz', 'jzz', 'gay', 'gey', 'gei', 'gai', 'vag', 'vgn', 'sjv', 'fap', 'prn', 'lol', 'jew', 'joo', 'gvr', 'pus', 'pis', 'pss', 'snm', 'tit', 'fku', 'fcu', 'fqu', 'hor', 'slt', 'jap', 'wop', 'kik', 'kyk', 'kyc', 'kyq', 'dyk', 'dyq', 'dyc', 'kkk', 'jyz', 'prk', 'prc', 'prq', 'mic', 'mik', 'miq', 'myc', 'myk', 'myq', 'guc', 'guk', 'guq', 'giz', 'gzz', 'sex', 'sxx', 'sxi', 'sxe', 'sxy', 'xxx', 'wac', 'wak', 'waq', 'wck', 'pot', 'thc', 'vaj', 'vjn', 'nut', 'std', 'lsd', 'poo', 'azn', 'pcp', 'dmn', 'orl', 'anl', 'ans', 'muf', 'mff', 'phk', 'phc', 'phq', 'xtc', 'tok', 'toc', 'toq', 'mlf', 'rac', 'rak', 'raq', 'rck', 'sac', 'sak', 'saq', 'pms', 'nad', 'ndz', 'nds', 'wtf', 'sol', 'sob', 'fob', 'sfu']
 func _ready() -> void:
 	spot_array = [
 		spot_1,
 		spot_2,
 		spot_3
 	]
-	
-	
-	
+
+
+
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("p1_l1"): #U
+	if event.is_action_pressed("p1_l1"): # U
 		spot_manager()
 	elif event.is_action_pressed("p1_a"): # H
 		_one_key_keyboard()
-	
-func spot_manager():
-	
-	spot_array[curr_spot].modulate = opaque
-	
+	elif event.is_action_pressed("p1_r1"): # I
+		filter_names()
 
-	curr_spot = curr_spot + 1 
-	
+func spot_manager():
+
+	spot_array[curr_spot].modulate = opaque
+
+	save_name()
+	curr_spot = curr_spot + 1
 	if curr_spot >= 3:
 		curr_spot = 0
-	save_name()
+	current_letter = curr_name_index[curr_spot]
+	
 	spot_updated = false
 	spot_array[curr_spot].modulate = half_opaque
 	flash = true
 	timer.start()
 	_on_timer_timeout()
-	
-	
+
+
 func _on_timer_timeout() -> void:
 	if flash: #true
 		flash = !flash
 		spot_array[curr_spot].modulate = half_opaque
-	else: 
+	else:
 		flash = !flash #false
 		spot_array[curr_spot].modulate = opaque
 	timer.start()
-	
-	
-	
-	
+
+
+
+
 func _one_key_keyboard():
-	"""
-	if spot_updated == false:
-		
-		var current_char = curr_name[curr_spot]
-		if current_char == '_':
-			alphebet_index = 0
-		else:
-			alphebet_index = alphabet.find(current_char)
-		print(str(alphebet_index))
-		
-		current_letter = alphebet_index
-		spot_updated = true
-	else:
-		pass
-	spot_array[curr_spot].text = str(alphabet[current_letter])
-	current_letter += 1
-	if current_letter >= 26:
-		current_letter = 0
-	"""
-	if spot_updated  == false:
-		current_letter = curr_name_index[curr_spot]
-		spot_updated = true
-	else:
-		pass
-	
+
 	spot_array[curr_spot].text = str(alphabet[current_letter])
 	current_letter += 1
 	print(str(current_letter))
-	if current_letter >= 26:
+	if current_letter >= max_letters:
 		current_letter = 0
-	
-	
-	
-	
-func _process(_delta: float) -> void:
-	pass
-	
-	
+
 func save_name():
 	curr_name_index[curr_spot] = current_letter #saves the index of the current name to the current letter before switching
-	curr_name[curr_spot] = alphabet[current_letter]
-	current_letter = 0 
-	for i in curr_name_index:
+	#curr_name[curr_spot] = alphabet[current_letter]
+	
+
+
+func filter_names():
+	for i in range(len(spot_array)):
+		curr_name.append(spot_array[i].text)
+	var to_be_filtered_name  =''.join(curr_name)
+	print(to_be_filtered_name)
+	
+	if to_be_filtered_name.to_lower() in nono_words:
+		print("what the sigma man")
+		animation_player.play("bad_word")
+		timer.stop()
+		spot_array[curr_spot].modulate = opaque
+		curr_spot = 0
 		
-		print("current number: " + str(i) + " is " + str(current_letter[i])) 
-	
-	
+		curr_name_index =  [0, 0, 0]
+		await animation_player.animation_finished
+		for i in range(len(spot_array)):
+			spot_array[i].text = '_'
+		timer.start()
+		
+	curr_name = []
+
+
+
+
+
+
+func _process(_delta: float) -> void:
+	pass
--- scenes/Characters/chill_wizard.gd
+++ scenes/Characters/chill_wizard.gd
@@ -5,17 +5,17 @@ class_name player
 @onready var main = get_tree().get_root()
 
 @export var projectile : PackedScene
-@export var projectile_2 : PackedScene 
+@export var projectile_2 : PackedScene
 var current_projectile = 0
 
-@onready var marker_2d: Marker2D = $Marker2D
+@onready var marker_2d: Marker2D = $Marker2Dz
 
 @onready var GAME_HUD = $"../../game_hud"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var bullet_sound_effect_player: AudioStreamPlayer = $BulletSoundEffectPlayer
 @onready var player_bullets: Node2D = $"../../BulletManager/PlayerBullets"
 @onready var timer: Timer = $Timer
- 
+
 signal player1_died
 
 var shoot_effect_one = preload("uid://lq088uvjrlrj")
@@ -53,7 +53,7 @@ func shoot():
 		bullet.global_position = marker_2d.global_position
 		bullet.rotation = global_rotation
 		player_bullets.add_child(bullet)
-	
+
 		bullet_sound_effect_player.stream = shoot_effect_one
 		bullet_sound_effect_player.play()
 	else:
@@ -64,7 +64,7 @@ func health_manager(change: int):
 	GAME_HUD.set_health(health)
 	if health <= 0:
 		player1_died.emit
-		
+
 
 
 
@@ -76,5 +76,5 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		else:
 			animation_player.play("on_hit")
 			health_manager(-1)
-		#sigma 
+		#sigma
 		area.queue_free()
