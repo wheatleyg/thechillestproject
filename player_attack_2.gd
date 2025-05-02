49ce5133cf0a55809e86206cc756737456995d35
JoshuaxLasley <jlasley@ftstudent.org>
2025-05-02 12:15:56 -0400

    Writing bullet script

--- player_attack_2.gd
+++ player_attack_2.gd
@@ -1,5 +1,13 @@
 extends Player_attack
 
+func _ready() -> void:
+	velocity.y = -SPEED
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
 
 
-class_name player_attack_2
+func _physics_process(delta: float) -> void:
+	move_and_slide()
